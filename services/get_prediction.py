cec10dd63fb101a77dd7d1c1b6dd9aac5f197db3
NirmalSankalana <sankalana.nirmal@gmail.com>
2023-09-13 21:45:37 +0530

    add new model

--- services/get_prediction.py
+++ services/get_prediction.py
@@ -1,8 +1,9 @@
 import joblib
 
-MODEL_PATH = '../model/hybrid_model3.pkl'
+MODEL_PATH = '../model/inceptiontime_model.pkl'
 
 model = joblib.load(MODEL_PATH)
+
 
 def get_predictions(df):
     predictions = model.predict(df)
